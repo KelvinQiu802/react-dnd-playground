eed33bb7fabfac124b59e1d44b758c3ea7b0f0e5
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-15 13:20:55 +0800

    feat: title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './styles/App.css';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div className='App'>
+      <h1 className='title'>React Drag & Drop Playground</h1>
+    </div>
+  );
 }
 
 export default App;
