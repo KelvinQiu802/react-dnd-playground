e303407ad81c1eb1e5255aecd40dae28f57c9ad1
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-15 14:26:42 +0800

    feat: add more draggable

--- src/components/Eg1.jsx
+++ src/components/Eg1.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import Draggable from './Draggable';
+import DragGroup from './DragGroup';
 import Droppable from './Droppable';
 
 function Eg1() {
-  const handleDrop = () => {
-    alert('You drop it!');
+  const handleDrop = (item) => {
+    alert(`You drop ${item.name}!`);
   };
+
   return (
     <>
-      <Draggable type='drag1' text='Drag Me' />
+      <DragGroup>
+        <Draggable type='drag1' text='React' item={{ name: 'React' }} />
+        <Draggable type='drag1' text='Vue' item={{ name: 'Vue' }} />
+        <Draggable type='drag1' text='Angular' item={{ name: 'Angular' }} />
+      </DragGroup>
       <Droppable accept='drag1' handleDrop={handleDrop} text='Drop it Here!' />
     </>
   );
