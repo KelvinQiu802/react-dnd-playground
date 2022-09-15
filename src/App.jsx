a4201efd8e14d310029b1e87293a381c46fc21f3
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-15 13:23:33 +0800

    feat: Content component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Content from './components/Content';
 import './styles/App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1 className='title'>React Drag & Drop Playground</h1>
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../styles/Content.module.css';
+
+function Content() {
+  return <div className={styles.content}>Content</div>;
+}
+
+export default Content;
