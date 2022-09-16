ca65b90c9c5adea8094afabed531d49e13e3250a
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-16 10:36:36 +0800

    feat: Eg5

--- src/components/Eg5.jsx
+++ src/components/Eg5.jsx
@@ -3,16 +3,26 @@ import Draggable from './Draggable';
 import Droppable from './Droppable';
 
 function Eg5() {
-  const handleDrop = () => {};
+  const handleDrop = (item, monitor, state) => {
+    alert(`You drop ${item.name}!`);
+  };
   return (
     <>
-      <Draggable type='drag-5' text='Cat'>
-        <Draggable type='drag-5' text='British'>
-          <Draggable type='drag-5' text='Shorthair' />
-          <Draggable type='drag-5' text='Longhair' />
+      <Draggable type='drag-5' text='Cat' item={{ name: 'Cat' }}>
+        <Draggable type='drag-5' text='British' item={{ name: 'British' }}>
+          <Draggable
+            type='drag-5'
+            text='Shorthair'
+            item={{ name: 'Shorthair' }}
+          />
+          <Draggable
+            type='drag-5'
+            text='Longhair'
+            item={{ name: 'Longhair' }}
+          />
         </Draggable>
       </Draggable>
-      <Droppable accept='drag-5' handleDrop={handleDrop}></Droppable>
+      <Droppable accept='drag-5' handleDrop={handleDrop} text='Drop Here' />
     </>
   );
 }
