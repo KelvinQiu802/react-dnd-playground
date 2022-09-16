09ab56f3242fb6d0f5722a0a4aeef292f2f9d810
KelvinQiu <kelvinqiu802@outlook.com>
2022-09-16 14:40:58 +0800

    feat: sort functino

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,59 @@
 import React from 'react';
+import { useDrop, useDrag } from 'react-dnd';
 import styles from '../styles/Card.module.css';
 
 function Card({ index, id, text, handleDrag }) {
-  return <div className={styles.draggable}>{text}</div>;
+  const ref = React.useRef(null);
+
+  const [{ isDragging }, drag] = useDrag(() => ({
+    type: 'card',
+    item: { id, index },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  }));
+
+  const [{ handlerId }, drop] = useDrop(() => ({
+    accept: 'card',
+    collect: (monitor) => ({
+      handlerId: monitor.getHandlerId(),
+    }),
+    hover: (item, monitor) => {
+      if (!ref.current) return;
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      // Do nothing if target and source are same
+      if (dragIndex === hoverIndex) return;
+
+      const hoverRect = ref.current.getBoundingClientRect();
+      // Get vertical middle
+      const hoverMiddleY = (hoverRect.bottom - hoverRect.top) / 2;
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverRect.top;
+
+      // Only move when the mouse has crossed half of the items height
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+
+      handleDrag(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  }));
+
+  const opacity = isDragging ? 0 : 1;
+
+  drag(drop(ref));
+  return (
+    <div className={styles.draggable} ref={ref} style={{ opacity }}>
+      {text}
+    </div>
+  );
 }
 
 export default Card;
--- src/components/Eg7.jsx
+++ src/components/Eg7.jsx
@@ -29,10 +29,27 @@ function Eg7() {
     },
   ]);
 
+  const handleDrag = (dragIndex, hoverIndex) => {
+    setCards((prev) => {
+      const copy = [...prev];
+      const card = copy[dragIndex];
+      // remove origin
+      copy.splice(dragIndex, 1);
+      // add to target
+      copy.splice(hoverIndex, 0, card);
+      return copy;
+    });
+  };
+
   return (
     <div>
       {cards.map((item, index) => (
-        <Card key={item.id} index={index} text={item.text} />
+        <Card
+          key={item.id}
+          index={index}
+          text={item.text}
+          handleDrag={handleDrag}
+        />
       ))}
     </div>
   );
